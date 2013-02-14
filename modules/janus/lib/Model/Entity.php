0883dd104c2be0f8f40cd13902c804f692301566
Lucas van Lierop <lucas.vanlierop@flex.surfnet.nl>
2013-02-14 14:02:46 +0100

    Added getters and setters

--- modules/janus/lib/Model/Entity.php
+++ modules/janus/lib/Model/Entity.php
@@ -116,4 +116,214 @@ class sspmod_janus_Model_Entity {
      * @ORM\Column(name="active", nullable=false)
      */
     protected $active = 'yes';
+
+    public function setActive($active)
+    {
+        $this->active = $active;
+        return $this;
+    }
+
+    public function getActive()
+    {
+        return $this->active;
+    }
+
+    public function setAllowedAll($allowedAll)
+    {
+        $this->allowedAll = $allowedAll;
+        return $this;
+    }
+
+    public function getAllowedAll()
+    {
+        return $this->allowedAll;
+    }
+
+    public function setArp($arp)
+    {
+        $this->arp = $arp;
+        return $this;
+    }
+
+    public function getArp()
+    {
+        return $this->arp;
+    }
+
+    public function setCreated($created)
+    {
+        $this->created = $created;
+        return $this;
+    }
+
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    public function setEid($eid)
+    {
+        $this->eid = $eid;
+        return $this;
+    }
+
+    public function getEid()
+    {
+        return $this->eid;
+    }
+
+    public function setEntityId($entityId)
+    {
+        $this->entityId = $entityId;
+        return $this;
+    }
+
+    public function getEntityId()
+    {
+        return $this->entityId;
+    }
+
+    public function setExpiration($expiration)
+    {
+        $this->expiration = $expiration;
+        return $this;
+    }
+
+    public function getExpiration()
+    {
+        return $this->expiration;
+    }
+
+    public function setIp($ip)
+    {
+        $this->ip = $ip;
+        return $this;
+    }
+
+    public function getIp()
+    {
+        return $this->ip;
+    }
+
+    public function setManipulation($manipulation)
+    {
+        $this->manipulation = $manipulation;
+        return $this;
+    }
+
+    public function getManipulation()
+    {
+        return $this->manipulation;
+    }
+
+    /**
+     * @param \DateTime $metadataCacheUntil
+     */
+    public function setMetadataCacheUntil(\DateTime $metadataCacheUntil)
+    {
+        $this->metadataCacheUntil = $metadataCacheUntil;
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getMetadataCacheUntil()
+    {
+        return $this->metadataCacheUntil;
+    }
+
+    public function setMetadataUrl($metadataUrl)
+    {
+        $this->metadataUrl = $metadataUrl;
+        return $this;
+    }
+
+    public function getMetadataUrl()
+    {
+        return $this->metadataUrl;
+    }
+
+    /**
+     * @param \DateTime $metadataValidUntil
+     */
+    public function setMetadataValidUntil(\DateTime $metadataValidUntil)
+    {
+        $this->metadataValidUntil = $metadataValidUntil;
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getMetadataValidUntil()
+    {
+        return $this->metadataValidUntil;
+    }
+
+    public function setParent($parent)
+    {
+        $this->parent = $parent;
+        return $this;
+    }
+
+    public function getParent()
+    {
+        return $this->parent;
+    }
+
+    public function setRevisionId($revisionId)
+    {
+        $this->revisionId = $revisionId;
+        return $this;
+    }
+
+    public function getRevisionId()
+    {
+        return $this->revisionId;
+    }
+
+    public function setrevisionNote($revisionNote)
+    {
+        $this->revisionNote = $revisionNote;
+        return $this;
+    }
+
+    public function getrevisionNote()
+    {
+        return $this->revisionNote;
+    }
+
+    public function setState($state)
+    {
+        $this->state = $state;
+        return $this;
+    }
+
+    public function getState()
+    {
+        return $this->state;
+    }
+
+    public function setType($type)
+    {
+        $this->type = $type;
+        return $this;
+    }
+
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    public function setUser($user)
+    {
+        $this->user = $user;
+        return $this;
+    }
+
+    public function getUser()
+    {
+        return $this->user;
+    }
 }
