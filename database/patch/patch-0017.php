6fe9fb9e336f41aaefe29a8ba09d67b620520176
François Kooman <fkooman@tuxed.net>
2013-03-27 11:18:20 +0100

    add database patch to use new UIInfo names

---
+++ database/patch/patch-0017.php
@@ -0,0 +1,60 @@
+<?php
+// Rename MDUI / UIInfo metadata fields (BACKLOG-974)
+
+/**
+ * DbPatch makes the following variables available to PHP patches:
+ *
+ * @var $this       DbPatch_Command_Patch_PHP
+ * @var $writer     DbPatch_Core_Writer
+ * @var $db         Zend_Db_Adapter_Abstract
+ * @var $phpFile    string
+ */
+
+$_SERVER['REMOTE_ADDR'] = '127.0.0.1';
+
+$janusConfig = SimpleSAML_Configuration::getConfig('module_janus.php');
+
+$userController = new sspmod_janus_UserController($janusConfig);
+$userController->setUser('engine');
+$entities = $userController->getEntities();
+/** @var sspmod_janus_Entity $entity */
+foreach ($entities as $entity) {
+    $entity->setRevisionnote('patch-0017.php: update MDUI/UIInfo metadata fields (BACKLOG-974)');
+
+    $entityController = new sspmod_janus_EntityController($janusConfig);
+    $entityController->setEntity($entity);
+
+    $metadata = $entityController->getMetadata();
+
+    foreach($metadata as $md) {
+        if('displayName:en' === $md->getKey()) {
+            $entityController->addMetadata('UIInfo:DisplayName:en', $md->getValue());
+            $entityController->removeMetadata('displayName:en');
+        }
+        if('displayName:nl' === $md->getKey()) {
+            $entityController->addMetadata('UIInfo:DisplayName:nl', $md->getValue());
+            $entityController->removeMetadata('displayName:nl');
+        }
+        if('keywords:en' === $md->getKey()) {
+            $entityController->addMetadata('UIInfo:Keywords:en', $md->getValue());
+            $entityController->removeMetadata('keywords:en');
+        }
+        if('keywords:nl' === $md->getKey()) {
+            $entityController->addMetadata('UIInfo:Keywords:nl', $md->getValue());
+            $entityController->removeMetadata('keywords:nl');
+        }
+        if('logo:0:url' === $md->getKey()) {
+            $entityController->addMetadata('UIInfo:Logo:0:url', $md->getValue());
+            $entityController->removeMetadata('logo:0:url');
+        }
+        if('logo:0:width' === $md->getKey()) {
+            $entityController->addMetadata('UIInfo:Logo:0:width', $md->getValue());
+            $entityController->removeMetadata('logo:0:width');
+        }
+        if('logo:0:height' === $md->getKey()) {
+            $entityController->addMetadata('UIInfo:Logo:0:height', $md->getValue());
+            $entityController->removeMetadata('logo:0:height');
+        }
+    }
+    $entityController->saveEntity();
+}
