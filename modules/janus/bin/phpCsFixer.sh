5d6dd662e3a5770ba9656ed53d28d6d916114b2f
lucasvanlierop <lucas@vanlierop.org>
2013-06-13 16:45:40 +0200

    Addes comments to qa scripts

--- modules/janus/bin/phpCsFixer.sh
+++ modules/janus/bin/phpCsFixer.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Runs the PHP code style fixer on the most important code
 PARENT_DIR=$(pwd);
 $PARENT_DIR/vendor/bin/php-cs-fixer fix lib
 $PARENT_DIR/vendor/bin/php-cs-fixer fix www
--- modules/janus/bin/phpmd.sh
+++ modules/janus/bin/phpmd.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-    PARENT_DIR=$(pwd);
+# Runs the PHP mess detector on the most important code
+PARENT_DIR=$(pwd);
 $PARENT_DIR/vendor/bin/phpmd lib/,templates,www text codesize
 #,unusedcode,naming
